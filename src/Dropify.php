e3a1b3affdc34304a678e9f00313ee871f49cade
Sugeng Sulistiyawan <sugeng.sulistiyawan@gmail.com>
2020-03-10 18:54:43 +0700

    Add preview for type .ico, .webp

--- src/Dropify.php
+++ src/Dropify.php
@@ -33,6 +33,14 @@ class Dropify extends InputWidget
      */
     public function init()
     {
+        /**
+         * Add preview for type .ico, .webp
+         * Fixed Issue https://github.com/die-coding/yii2-dropify/issues/1
+         */
+        if (empty($this->pluginOptions['imgFileExtensions'])) {
+            $this->pluginOptions['imgFileExtensions'] = ['png', 'jpg', 'jpeg', 'gif', 'bmp', 'ico', 'webp'];
+        }
+
         $this->_options = [
             "class" => "dropify",
         ];
